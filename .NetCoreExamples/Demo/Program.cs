3d3a464b4c72774473d08d8aec8b95b649455c05
Datta98 <56407906+Datta98@users.noreply.github.com>
2022-01-18 10:18:42 +0530

    CoreDemo

---
+++ .NetCoreExamples/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorLib.Calculator calculator = new CalculatorLib.Calculator();
+            int result=  calculator.Add(5,10);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
